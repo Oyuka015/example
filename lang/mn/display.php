c195ff25ff7e3f093a67f7cf170167adcdf3f149
chappie <39295884+bumaa7018@users.noreply.github.com>
2023-01-26 10:05:52 +0800

    4

---
+++ lang/mn/display.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'information_register' => 'Мэдээлэл бүртгэх',
+    'online_course_register' => 'Цахим хичээл бүртгэх',
+    'exam' => 'Шалгалт',
+    'certificate' => 'Гэрчилгээ',
+    'certificate_register' => 'Гэрчилгээ бүртгэх',
+    'certificate_id' => 'Гэрчилгээний дугаар',
+    'faq_register' => 'Асуулт хариулт бүртгэх',
+    'feedback' => 'Санал хүсэлт',
+    'feedback_list' => 'Санал хүсэлтийн жагсаалт',
+    'filter_search' => 'Шүүж харах',
+    'search' => 'Хайх',
+    'add_new' => 'Шинээр нэмэх',
+    'image' => 'Зураг',
+    'title' => 'Гарчиг',
+    'information' => 'Мэдээлэл',
+    'manage' => 'Удирдах',
+    'close' => 'Хаах',
+    'save' => 'Хадгалах',
+    'edit' => 'Засварлах',
+    'delete' => 'Устгах',
+    'home' => 'Нүүр',
+    'settings' => 'Тохиргоо',
+
+    'name' => 'Нэр',
+    'user_name' => 'Хэрэглэгчийн нэр',
+    'login_name' => 'Нэвтрэх нэр',
+    'family_name' => 'Ургийн овог',
+    'lastname' => 'Нэр',
+    'surname' => 'Овог',
+    'lesson_name' => 'Хичээлийн нэр',
+    'exam_name' => 'Шалгалтын нэр',
+
+    'lesson' => 'Хичээл',
+    'lessons' => 'Хичээлүүд',
+    'lesson_summary' => 'Хичээлийн товч мэдээлэл',
+    'lesson_posted' => 'Хичээл оруулсан',
+    'lesson_type' => 'Хичээлийн төрөл',
+
+    'posted_date' => 'Хадгалсан огноо',
+    'registered_date' => 'Бүртгэсэн огноо',
+    'question' => 'Асуулт',
+    'answer' => 'Хариулт',
+    'exam_file' => 'Шалгалтын материал',
+    'exam_takers' => 'Шалгалтанд хамрагдагсад',
+    'code' => 'Код',
+    'register' => 'Регистер',
+    'registered_user' => 'Бүртгэсэн хэрэглэгч',
+    'valid_for' => 'Хүчинтэй хугацаа',
+    'signature' => 'Гарын үсэг',
+    'phone' => 'Утас',
+    'email' => 'И-Мейл',
+    'citizenship' => 'Иргэншил',
+    'active_status' => 'Идэвхтэй эсэх',
+    'system_user' => 'Системийн хэрэглэгч',
+    'province' => 'Аймаг',
+    'address' => 'Хаяг',
+    'home_address' => 'Гэрийн хаяг',
+    'age' => 'Нас',
+    'gender' => 'Хүйс',
+    'male' => 'Эрэгтэй',
+    'female' => 'Эмэгтэй',
+    'password' => 'Нууц үг',
+    'change_image' => 'Зураг солих',
+
+
+
+];
---
+++ lang/mn/messages.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'warning_title' => 'Анхааруулага',
+    'validation_field_required' => 'Заавал оруулах шаардлагатай!',
+    'error_save' => 'Хадагалах үед алдаа гарлаа',
+    'success_save' => 'Амжилттай хадгалллаа',
+    'success_delete' => 'Амжилттай устгалаа',
+    'confirm_delete_content' => 'Мэдээллийг устгахдаа итгэлтэй байна уу?',
+    'table_empty' => 'Мэдээлэл байхгүй',
+    'table_showing' => 'Нийт _TOTAL_ мэдээллээс  _START_ - _END_ хүртэл харуулж байна',
+    'table_showing_empty' => 'Нийт 0 мэдээлэл байна',
+    'table_filtered' => '(Нийт _MAX_ мэдээллээс шүүв)',
+    'table_processing' => 'Ачааллаж байна',
+    'table_no_match' => 'Мэдээлэл байхгүй байна',
+    'table_first' => 'Эхнийх',
+    'table_last' => 'Сүүлчийн',
+    'table_next' => 'Дараах',
+    'table_previous' => 'Өмнөх',
+    'no_result' => 'Үр дүн олдсонгүй!',
+    'image_not_found' => 'Зураг оруулаагүй байна!'
+];
